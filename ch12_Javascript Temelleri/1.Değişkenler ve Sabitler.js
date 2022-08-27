d3855211a1dd81154b8f022c836f6f2a53815c69
Sdrs81 <serdardemirtas25@gmail.com>
2022-08-28 02:30:25 +0300

    Değişkenler ve sabitler

---
+++ ch12_Javascript Temelleri/1.Değişkenler ve Sabitler.js
@@ -0,0 +1,21 @@
+// Değişkenler
+
+var password;
+console.log(password)
+
+password = "levent"
+console.log(password)
+
+yas = 25;
+console.log(yas)
+
+
+// Değişken tanımlama Kuralları
+
+// 1- Değişken isimleri rakam ile başlayamaz
+// 2- Komutlar değişken ismi olarak kullanılamaz
+// 3- Değişken ismi arasında boşluk bırakılamaz (ad_soyad, adSoyad şeklinde olabilir)
+// 4- Türkçe karakter kullanılmamalı
+// 5- const = sabit, değeri değişmez. (var gibi tanımlamada kullanılır)
+
+
