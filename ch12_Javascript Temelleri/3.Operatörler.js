2e7fbf0c29ea48a1d296cd198030d25f612ac2c6
Sdrs81 <serdardemirtas25@gmail.com>
2022-08-29 15:43:17 +0300

    Operatörler

---
+++ ch12_Javascript Temelleri/3.Operatörler.js
@@ -0,0 +1,39 @@
+
+// 1. Aritmetik Operatörler
+
+// + - / * %
+
+// variable++, -- or ++variable       
+// if data = variable++  ==> data is smaller than varibale
+// if data = ++variable  ==> data is the same with variable
+
+
+// 2. Atama Operatörleri
+
+let data = 0;
+let a = 5;
+let b = 4;
+
+veri = a;
+veri += a; // veri = veri + a; 
+veri -= a; // veri = veri - a;
+veri *= a; // veri = veri * a;
+veri /= a; // veri = veri / a;
+veri %= a; // veri = veri % a;
+
+
+// 3. Karşılaştırma Operatörleri
+
+veri = a == b; // is equal?, that return true or false so veri is boolean value
+// also doesn't check for types, so --> data = 5 == "5" is true for data
+
+veri = a === b; //but in this check also types so --> data = 5 === "5" is false for data
+
+// != !== < > <= >=
+
+// x!==y is equal wit this !(x === y)
+
+
+// 4. Mantıksal Operatörler
+
+// && || !
