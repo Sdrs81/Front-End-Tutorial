a0d4f1926f9169b4af234237bdafceac0337f496
Sdrs81 <serdardemirtas25@gmail.com>
2022-08-29 14:39:28 +0300

    Değişken Türleri

---
+++ ch12_Javascript Temelleri/2.Değişken Türleri.js
@@ -0,0 +1,41 @@
+
+// Primitve types
+
+// String
+let firstName = "levent"
+console.log(typeof firstName)
+
+// Number
+let age = 30;
+console.log(typeof age);
+
+// Boolean
+let krediKUllanımı = false;
+console.log(typeof krediKUllanımı);
+
+// undefined
+let phone
+console.log(typeof phone);
+
+
+// Reference Types: Objects
+
+// Array
+let liste = ["Aslı","Sertaç","Kerem"]
+console.log(typeof liste);
+
+// Object Literals
+let address = {
+    city: "İstanbul",
+    country: "Turkey",
+}
+console.log(typeof address);
+
+// Function
+var hesapla = function(){
+    return 0;
+}
+console.log(typeof hesapla);
+
+
+
