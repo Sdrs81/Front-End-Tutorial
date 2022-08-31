955bae2d7721d818b92894253d71554f883d50c0
Sdrs81 <serdardemirtas25@gmail.com>
2022-09-01 02:44:50 +0300

    Scope (Kapsam) Kavramı

---
+++ ch15_Javascript - Fonksiyonlar/3.Scope (Kapsam) Kavramı.js
@@ -0,0 +1,45 @@
+
+
+// // ---- Scope Kavramı ---- \\
+
+// // Gloabal Scope \\
+
+// function a() {
+//     // Function csope
+// }
+
+// if (true) {
+//     // Block Scope
+// }
+
+
+// var veri1 = 5; // Global scope
+// let veri2 = 10;
+// const veri3 = 15;
+
+// function fonskiyon(){  // function scope > global scope , block scope > global scope
+//     var veri1 = 30;
+//     let veri2 = 40;
+//     const veri3 = 50;
+
+//     console.log(veri1,veri2,veri3);
+// }
+
+// fonskiyon();
+
+// console.log(veri1,veri2,veri3);  // Global scope
+
+
+
+// if(true) {
+//     var a = 10;   // In block scope, var is valid for general, other ones aren't 
+//     let b = 20;   // if we define var variable in upper lines, still this var variable (b)
+//     const c = 10;  // is set from if block, other ones goes with global values
+// }
+
+// console.log(a)  // it is working, not other ones
+// console.log(b)
+// console.log(c)
+
+
+
