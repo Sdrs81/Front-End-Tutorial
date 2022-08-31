50bf4c3feef5b8447a3bf1dd82a49df9059f4849
Sdrs81 <serdardemirtas25@gmail.com>
2022-08-31 03:07:08 +0300

    While, For, Do While Döngüleri

--- ch14_Javascript - Döngüler/1.While, For, Do While Döngüleri.js
+++ ch14_Javascript - Döngüler/1.While, For, Do While Döngüleri.js
@@ -1,5 +1,51 @@
 
 
+// while loop
+
+// let i = 0;
+
+// while(i<5) {
+//     console.log(i);
+//     i++;
+// }
 
 
+// break contiune
 
+// let i = 0;
+
+// while (i < 10) {
+//     console.log(i);
+//     if(i == 6){
+//         break
+//     }
+//     i++;
+// }
+
+// let i = 0;
+
+// while (i < 5) {
+//     if(i == 3){
+//         i++;
+//         continue
+//     }
+//     console.log(i);
+//     i++;
+// }
+
+
+// do while
+
+// let i = 0;
+
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<10);
+
+
+// for loop
+
+for(let i = 0; i<5; i++){
+    console.log(i);
+}
