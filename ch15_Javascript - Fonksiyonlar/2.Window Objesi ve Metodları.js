3ba532919def06eff0923629a51faa089ad099d8
Sdrs81 <serdardemirtas25@gmail.com>
2022-09-01 01:54:33 +0300

    Window Objesi ve Metodları

---
+++ ch15_Javascript - Fonksiyonlar/2.Window Objesi ve Metodları.js
@@ -0,0 +1,36 @@
+
+// Window objesi
+// let veri = window
+// console.log(veri);
+
+
+// alert
+//alert("Merhaba");
+
+// prompt
+// var veri2 = prompt("Adınızı Giriniz");
+// console.log("Adınız:" + veri2);
+
+// confirm
+// veri3 = confirm("Çıkmak isteiğinize mein misiniz?")
+// console.log(veri3)   // If click okey that returns true value, otherwise return false 
+
+// location
+// veri = window.location
+// console.log(veri)
+
+// veri = window.location.hostname
+// console.log(veri)
+
+//window.location.href="https://www.udemy.com"  // Goes to site that we want
+
+//window.location.reload();
+
+
+// navigator
+//window.navigator  // we should type this in browser console
+
+// document
+//window.document  // HTML vs. code of that website, we should type this in browser console
+
+
