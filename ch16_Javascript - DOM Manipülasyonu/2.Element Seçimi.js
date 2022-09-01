518eed43db29e2482757ca1370a3548a3410b3a8
Sdrs81 <serdardemirtas25@gmail.com>
2022-09-02 02:12:31 +0300

    Element Seçimi

---
+++ ch16_Javascript - DOM Manipülasyonu/2.Element Seçimi.js
@@ -0,0 +1,51 @@
+
+
+
+// // Tek element seçimi
+// // document.getElementById();
+
+// let veri;
+// veri = document.getElementById("header")
+// veri = document.getElementById("header").id;
+// veri = document.getElementById("header").className;
+
+// veri = document.getElementById("header");
+// veri = veri.id
+
+// veri = document.getElementById("header");
+// veri = veri.className
+
+// veri = document.getElementById("header");
+
+// veri.style.color = "blue";
+// veri.style.fontSize = "50px";
+// //veri.style.fontWeight = "bold"
+// //veri.style.display = "none";
+
+// document.getElementById("header").innerText = "Yapılacaklar";
+// document.getElementById("header").innerText = "<b> ToDo list </b>"
+
+// document.getElementById("header").innerHTML = "<b> ToDo list </b>"
+
+// console.log(veri);
+
+
+
+// document.querySelector metodu
+let veri
+
+//console.log(document.querySelector("#header"));
+//console.log(document.querySelector(".card-header"));
+//console.log(document.querySelector("div"));
+
+// document.querySelector("li").style.color = "blue";
+// document.querySelector("li:last-child").style.color = "red";
+// document.querySelector("li:nth-child(2)").style.color = "green";
+
+document.querySelector("li").className = "list-group-item list-group-item-primary";
+document.querySelector("li").classList.add("active");   // adding class
+
+
+
+
+
