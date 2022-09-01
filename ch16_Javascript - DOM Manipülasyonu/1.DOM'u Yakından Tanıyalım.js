e75861672724352dcee36710eb92bf12eb527ce8
Sdrs81 <serdardemirtas25@gmail.com>
2022-09-02 01:21:24 +0300

    1.DOM'u Yakından Tanıyalım

---
+++ ch16_Javascript - DOM Manipülasyonu/1.DOM'u Yakından Tanıyalım.js
@@ -0,0 +1,21 @@
+
+
+let veri;
+veri = window.document
+veri = window.document.head;
+veri = window.document.body;
+veri = window.document.URL;
+veri = window.document.domain;
+veri = window.document.images;
+veri = window.document.forms;
+veri = window.document.forms[0].id;
+veri = window.document.forms[0].method;
+veri = window.document.forms[0].action;
+veri = window.document.scripts;
+veri = window.document.scripts[2];
+
+
+
+
+
+console.log(veri);
