ae85378440ac39741458f5f4454f256f38f908b6
Sdrs81 <serdardemirtas25@gmail.com>
2022-09-05 21:26:26 +0300

    Event Listeners ve Event Objesi

--- ch17_Javascript - DOM Eventleri/1.Event Listeners ve Event Objesi.js
+++ ch17_Javascript - DOM Eventleri/1.Event Listeners ve Event Objesi.js
@@ -1,5 +1,35 @@
 
+const btn = document.querySelector("#btnDeleteAll");
+const btn2 = document.querySelector("#btnAddNewTask");
+
+
+// btn.addEventListener("click", function(){
+//     console.log("Butona tıklandı");
+// });
+
+// btn.addEventListener("click", btnClick);
+// btn2.addEventListener("click", btnClick);
+
+// function btnClick(){
+//     console.log("Butona tıklanıldı");
+// };
+
+btn.addEventListener("click", function(a){
+
+    let value;
+
+    value = a;
+    value = a.target;   // Which button that we clicked
+    value = a.target.id;
+    value = a.target.classList;
+    value = a.type;
+
+    console.log(value);
+
+    a.preventDefault();
+});
 
 
 
-console.log()
+
+
