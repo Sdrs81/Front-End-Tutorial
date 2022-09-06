c1ee3e347afa0cc200f3f5cfb8f0e810fef42953
Sdrs81 <serdardemirtas25@gmail.com>
2022-09-06 03:15:27 +0300

    Session Storage

--- ch18_Tarayıcıların Depolama Alanları/1.Session Storage.js
+++ ch18_Tarayıcıların Depolama Alanları/1.Session Storage.js
@@ -1,2 +1,26 @@
 
+// This storage type save values for session (if you close the tab, datas will be gone)
 
+const add = document.querySelector("#add");
+const del = document.querySelector("#delete");
+const clear = document.querySelector("#clear");
+
+const addkey = document.querySelector("#addkey");
+const addvalue = document.querySelector("#addvalue");
+const deletekey = document.querySelector("#deletekey");
+
+add.addEventListener("click", addItem);
+del.addEventListener("click", deleteItem);
+clear.addEventListener("click", clearItem);
+
+function addItem(e) {
+    sessionStorage.setItem(addkey.value, addvalue.value);
+}
+
+function deleteItem(e) {
+    sessionStorage.removeItem(deletekey.value);
+}
+
+function clearItem(e) {
+    sessionStorage.clear();
+}
